213e0fe9727ba54837af34a1c400f66d70d618d7
Mulya <mylyahigh@gmail.com>
2021-07-18 19:46:43 +0300

    sidebar, slider, new collection section started

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function () {
-  console.log("hhhh");
+  $(".header__btn").on("click", () => {
+    $(".rightside-menu").removeClass("rightside-menu--close");
+  });
+  $(".rightside-menu__close").on("click", () => {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+  $(".top__slider").slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
 });
